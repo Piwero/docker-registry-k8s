6398185de078d53ef226b4d954063913a44bf32a
Nacho Pinero <piwero@gmail.com>
2023-10-27 18:24:47 +0100

    Add sudo to docker command

--- 1-auth.sh
+++ 1-auth.sh
@@ -10,7 +10,7 @@ mkdir -p ${DESTINATION_FOLDER}
 echo ${REGISTRY_USER} >> ${DESTINATION_FOLDER}/registry-user.txt
 echo ${REGISTRY_PASS} >> ${DESTINATION_FOLDER}/registry-pass.txt
    	
-docker run --entrypoint htpasswd registry:2.7.0 \
+sudo docker run --entrypoint htpasswd registry:2.7.0 \
     -Bbn ${REGISTRY_USER} ${REGISTRY_PASS} \
     > ${DESTINATION_FOLDER}/htpasswd
       
