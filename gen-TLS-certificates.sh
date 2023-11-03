fc025b0e40b7e8c1990c0582045bcba752e814d3
Nacho Pinero <piwero@gmail.com>
2023-11-03 17:05:43 +0000

    Update paths for creds and certs

--- gen-TLS-certificates.sh
+++ gen-TLS-certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CERT_DIR=${HOME}/temp/docker-registry/cert
+CERT_DIR=${HOME}/temp/registry/cert
 
 mkdir -p ${CERT_DIR}
 openssl req -newkey rsa:2048 -nodes -keyout registry_auth.key -x509 -days 365 -out ${CERT_DIR}/registry_auth.crt
