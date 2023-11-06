3e52eff063e30f77a9ff39e90cef7ba830aefe93
Nacho Pinero <piwero@gmail.com>
2023-11-06 19:55:07 +0000

    Revert "Use configmap for injecting values"
    This reverts commit 1bc5b4be2fc4d64ca41dc3252b49b7da7fa7e126.

--- gen-TLS-certificates.sh
+++ gen-TLS-certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TLS_DIR=${HOME}/registry/tls
+CERT_DIR=${HOME}/registry/tls
 
-mkdir -m 777 -p ${TLS_DIR}
-openssl req -newkey rsa:2048 -nodes -keyout ${TLS_DIR}/registry_auth.key -x509 -days 365 -out ${TLS_DIR}/registry_auth.crt
+mkdir -m 777 -p ${CERT_DIR}
+openssl req -newkey rsa:2048 -nodes -keyout ${CERT_DIR}/registry_auth.key -x509 -days 365 -out ${CERT_DIR}/registry_auth.crt
--- gen-registry-user-pass.sh
+++ gen-registry-user-pass.sh
@@ -2,16 +2,16 @@
 
 REGISTRY_USER=${1:-admin}
 REGISTRY_PASS=${2:-registry}
-AUTH_DIR=${HOME}/registry/auth
+DESTINATION_FOLDER=${HOME}/registry/auth
 
 # Backup credentials to local files (in case you'll forget them later on)
-mkdir -m 777 -p ${AUTH_DIR}
+mkdir -m 777 -p ${DESTINATION_FOLDER}
 
-echo ${REGISTRY_USER} >> ${AUTH_DIR}/registry-user.txt
-echo ${REGISTRY_PASS} >> ${AUTH_DIR}/registry-pass.txt
+echo ${REGISTRY_USER} >> ${DESTINATION_FOLDER}/registry-user.txt
+echo ${REGISTRY_PASS} >> ${DESTINATION_FOLDER}/registry-pass.txt
 
 sudo docker run --entrypoint htpasswd registry:2.7.0 \
     -Bbn ${REGISTRY_USER} ${REGISTRY_PASS} \
-    > ${AUTH_DIR}/htpasswd
+    > ${DESTINATION_FOLDER}/htpasswd
 
-unset REGISTRY_USER REGISTRY_PASS
+unset REGISTRY_USER REGISTRY_PASS DESTINATION_FOLDER
