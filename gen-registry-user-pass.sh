2bd78e6022ca66efec6eba172a723f0b40f5af5f
Nacho Pinero <piwero@gmail.com>
2023-11-06 08:35:45 +0000

    Update volumes

--- gen-registry-user-pass.sh
+++ gen-registry-user-pass.sh
@@ -2,7 +2,7 @@
 
 REGISTRY_USER=${1:-admin}
 REGISTRY_PASS=${2:-registry}
-DESTINATION_FOLDER=${HOME}/temp/registry/creds
+DESTINATION_FOLDER= /tmp/registry/creds
 
 # Backup credentials to local files (in case you'll forget them later on)
 mkdir -p ${DESTINATION_FOLDER}
