ffc814258969e9a05d05e63d17cd2c9bea1f349e
Nacho Pinero <piwero@gmail.com>
2023-11-06 11:08:04 +0000

    Create key in correct path

--- gen-TLS-certificates.sh
+++ gen-TLS-certificates.sh
@@ -2,4 +2,4 @@
 CERT_DIR=/tmp/registry/creds
 
 mkdir -p ${CERT_DIR}
-openssl req -newkey rsa:2048 -nodes -keyout registry_auth.key -x509 -days 365 -out ${CERT_DIR}/registry_auth.crt
+openssl req -newkey rsa:2048 -nodes -keyout ${CERT_DIR}/registry_auth.key -x509 -days 365 -out ${CERT_DIR}/registry_auth.crt
